e13b635afc9fe64720feddb0c93fcdba89a4a378
Joao <joaohenrique9021@gmail.com>
2023-11-25 19:36:36 -0300

    feat: impements footer

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.sass']
+  styleUrls: ['./forms.component.sass'],
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+  enviarEmail() {
+    return alert('Enviado com sucesso!');
+  }
 
+  ngOnInit(): void {}
 }
